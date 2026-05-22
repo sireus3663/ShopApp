a5e14222435105b70127599eaef04cd3e9a9f594
Obamych <golosapovkirill2019@gmail.com>
2026-05-22 21:19:15 +0700

    В CartRepository: Переделал GetByUser, чтобы он выдавал список всех товаров, а не один. Добавил: CartService, FavoriteRepository и FavoriteService

---
+++ ShopProject/Db/FavoriteRepository.cs
@@ -0,0 +1,29 @@
+﻿using ShopProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopProject.Db
+{
+    public class FavoriteRepository : BaseRepository<Favorite>
+    {
+        public FavoriteRepository(AppDbContext context) : base(context) { }
+        public List<Favorite> GetByUser(Guid userId)
+        {
+            return _dbSet
+                .Where(f => f.UserId == userId)
+                .ToList();
+        }
+
+        public Favorite GetFavoriteItem(Guid userId, Guid productId)
+        {
+            return _dbSet
+                .FirstOrDefault(f => f.UserId == userId && f.ProductId == productId);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ ShopProject/Services/FavoriteService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ShopProject.Db;
+using ShopProject.Models;
+
+namespace ShopProject.Services
+{
+    public class FavoriteService
+    {
+        private readonly FavoriteRepository _favoriteRepository;
+
+        public FavoriteService(FavoriteRepository favoriteRepository)
+        {
+            _favoriteRepository = favoriteRepository;
+        }
+        public void ToggleFavorite(Guid userId, Guid productId)
+        {
+            var existingItem = _favoriteRepository.GetFavoriteItem(userId, productId);
+
+            if (existingItem != null)
+            {
+                _favoriteRepository.Delete(existingItem.Id);
+            }
+            else
+            {
+                var newFavorite = new Favorite
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = userId,
+                    ProductId = productId
+                };
+
+                _favoriteRepository.Add(newFavorite);
+            }
+
+            _favoriteRepository.Save();
+        }
+
+        public List<Guid> GetFavorites(Guid userId)
+        {
+            return _favoriteRepository.GetByUser(userId)
+                .Select(f => f.ProductId)
+                .ToList();
+        }
+    }
+}
