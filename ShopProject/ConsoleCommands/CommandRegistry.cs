9e94b9afc99fe690ce39338c5fe8d1eef374d646
amasireus <misha.orlenko1994@gmail.com>
2026-05-24 14:46:09 +0700

    чёт комит прошлый криво зафиксировал

--- ShopProject/ConsoleCommands/CommandRegistry.cs
+++ ShopProject/ConsoleCommands/CommandRegistry.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopProject.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,12 @@ namespace ShopProject.ConsoleCommands
     public class CommandRegistry
     {
         private readonly Dictionary<string, ICommand> _commands = new();
+        private readonly LoggerService _logger;
+
+        public CommandRegistry(LoggerService logger)
+        {
+            _logger = logger;
+        }
 
         public void Register(ICommand command)
         {
@@ -19,10 +26,37 @@ namespace ShopProject.ConsoleCommands
             _commands.TryGetValue(name.ToLower(), out var command);
             return command;
         }
+        public void Execute(string commandName, string[] args)
+        {
+            var command = Get(commandName);
 
+            if (command == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Команда не найдена");
+                Console.ResetColor();
+                return;
+            }
+
+            try
+            {
+                command.Execute(args);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine(
+                    $"Ошибка выполнения: {ex.Message}"
+                );
+                _logger.Error("ошибка выполнения команды", ex);
+
+                Console.ResetColor();
+            }
+        }
         public void ShowHelp()
         {
-            Console.WriteLine("\n=== Допуступные команды ===/n");
+            Console.WriteLine("\n=== Допуступные команды ===");
             foreach (var cmd in _commands.Values.Distinct().OrderBy(c => c.Name))
             {
                 Console.WriteLine($"  {cmd.Name,-12} - {cmd.Description}");
---
+++ ShopProject/ConsoleCommands/RegisterCommand.cs
@@ -0,0 +1,44 @@
+﻿using ShopProject.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProject.ConsoleCommands
+{
+    public class RegisterCommand : BaseCommand
+    {
+        private readonly UserService _users;
+
+        public override string Name =>
+            "register";
+
+        public override string Description =>
+            "Регистрация (name, email, password)";
+
+        public RegisterCommand(UserService users)
+        {
+            _users = users;
+        }
+
+        public override void Execute(string[] args)
+        {
+            var name = args[0];
+
+            var email = args[1];
+
+            var password = args[2];
+
+            var user = _users.Register(
+                    name,
+                    email,
+                    password
+                );
+
+            Success(
+                $"Создан {user.Name}"
+            );
+        }
+    }
+}
