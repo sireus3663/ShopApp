529d92195e7f02581a9ae77d9cfdbf642cb8db4b
Minri9669 <minrihka@gmail.com>
2026-06-13 17:16:57 +0700

    удалил неиспользуемые файлы

--- ShopProject/Migrations/RemoveAmountColumn.sql
+++
@@ -1,8 +0,0 @@
-﻿-- Migration: RemoveAmountColumn
--- Removes the Amount column from Products table
-
-ALTER TABLE IF EXISTS "products" DROP COLUMN IF EXISTS "Amount";
-
--- Update the EF Migration history
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-VALUES ('20260613060012_RemoveAmountColumn', '8.0.4');
